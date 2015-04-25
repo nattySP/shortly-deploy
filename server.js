8605c046217ba8e96c7e848989b9a80b026c09f1
Hack Reactor Students <accounts@hackreactor.com>
2015-04-25 12:24:18 -0700

    complete mongoDB refactor

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-var app = require('./server-config.js');
+var app = require('./server-config.js'),
+    mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/shortly'); // connect to mongo database named shortly
 
 
 app.listen(process.env.PORT || 3000);
